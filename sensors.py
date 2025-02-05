64d9e90ae010584fd38b6dfc3916ebedd40fd949
atill4 <atill4@uic.edu>
2025-02-05 10:02:16 -0600

    Aaron's sensors file

--- sensors.py
+++ sensors.py
@@ -5,7 +5,7 @@ Description: This script <>
 Author: Josh Campbell <jcampb36@uic.edu>,
         Aaron Tillery <atill4@uic.edu>
 Date Created: 2025-01-28
-Last Modified: 2025-01-28
+Last Modified: 2025-02-05
 Version: 0.1
 Python Version: 3.11.1
 
